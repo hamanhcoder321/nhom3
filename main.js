7b182efe07ac614c29d9833b28f775d3f4155faa
webbanhang.manhcoder <vanmanh8930@gmail.com>
2024-10-08 13:56:44 +0700

    build view

--- main.js
+++ main.js
@@ -13,4 +13,25 @@ closeBtn.addEventListener('click', function () {
   mobileSidebar.classList.remove('active');
 });
 
-  
+// Lấy phần modal và các nút
+const loginModal = document.getElementById('loginModal');
+const loginIcon = document.querySelector('.icons a i.fa-user');
+const loginCloseBtn = document.getElementById('loginCloseBtn');
+
+// Khi click vào icon user, hiển thị modal
+loginIcon.addEventListener('click', function(e) {
+  e.preventDefault();
+  loginModal.style.display = 'flex';
+});
+
+// Khi click vào nút x, đóng modal
+loginCloseBtn.addEventListener('click', function() {
+  loginModal.style.display = 'none';
+});
+
+// Khi click ra ngoài modal, cũng đóng modal
+window.addEventListener('click', function(event) {
+  if (event.target === loginModal) {
+    loginModal.style.display = 'none';
+  }
+});
